ed78518b70acc0ec59a21e2d736e59165b8ab6b1
Jorge Vega <83831472+HX-JVega@users.noreply.github.com>
2022-12-09 14:58:34 -0300

    Add files via upload

--- DS.js
+++ DS.js
@@ -1,27 +1,19 @@
-function Queue() {
-  this.array = [];
-}
-
-Queue.prototype.enqueue = function(elemento) {
-  return this.array.push(elemento);
-}
-
-Queue.prototype.dequeue = function() {
+Queue.prototype.dequeue = function () {
   return this.array.shift();
-}
+};
 
-Queue.prototype.size = function() {
+Queue.prototype.size = function () {
   return this.array.length;
-}
+};
 
 function LinkedList() {
   this.head = null;
 }
 
-LinkedList.prototype.add = function(valor) {
+LinkedList.prototype.add = function (valor) {
   var nuevoNodo = new Node(valor);
 
-  if(!this.head){
+  if (!this.head) {
     this.head = nuevoNodo;
   } else {
     var tailActual = this.head;
@@ -30,14 +22,14 @@ LinkedList.prototype.add = function(valor) {
     }
     tailActual.next = nuevoNodo;
   }
-}
+};
 
-LinkedList.prototype.remove = function() {
-  if(!this.head){
+LinkedList.prototype.remove = function () {
+  if (!this.head) {
     return undefined;
   }
 
-  if(this.head.next === null){
+  if (this.head.next === null) {
     var unicoNodo = this.head;
     this.head = null;
     return unicoNodo.value;
@@ -51,30 +43,30 @@ LinkedList.prototype.remove = function() {
   }
   nodoPrevious.next = null;
   return nodoActual.value;
-}
+};
 
-LinkedList.prototype.search = function(arg) {
+LinkedList.prototype.search = function (arg) {
   var nodoActual = this.head;
 
-  if(nodoActual === null){
+  if (nodoActual === null) {
     return null;
   }
 
   while (nodoActual !== null) {
-    if(typeof arg === "function"){
-      if(arg(nodoActual.value)){
+    if (typeof arg === "function") {
+      if (arg(nodoActual.value)) {
         return nodoActual.value;
       }
-    } else if(nodoActual.value === arg){
-        return nodoActual.value;
+    } else if (nodoActual.value === arg) {
+      return nodoActual.value;
     }
     nodoActual = nodoActual.next;
   }
 
   return null;
-}
+};
 
-function Node(valor){
+function Node(valor) {
   this.value = valor;
   this.next = null;
 }
@@ -85,49 +77,47 @@ function BinarySearchTree(valor) {
   this.right = null;
 }
 
-BinarySearchTree.prototype.insert = function(value) {
-
-  if(value < this.value){
-    if(this.left === null){
+BinarySearchTree.prototype.insert = function (value) {
+  if (value < this.value) {
+    if (this.left === null) {
       var newTree = new BinarySearchTree(value);
       this.left = newTree;
     } else {
       this.left.insert(value);
     }
   } else {
-    if(this.right === null){
+    if (this.right === null) {
       var newTree = new BinarySearchTree(value);
       this.right = newTree;
     } else {
       this.right.insert(value);
     }
   }
-}
+};
 
-BinarySearchTree.prototype.size = function() {
-  if(this.value === null){
+BinarySearchTree.prototype.size = function () {
+  if (this.value === null) {
     return 0;
   }
 
-  if(this.left === null && this.right === null){
+  if (this.left === null && this.right === null) {
     return 1;
   }
 
-  if(this.left === null){
+  if (this.left === null) {
     return 1 + this.right.size();
   }
 
-  if(this.right === null){
+  if (this.right === null) {
     return 1 + this.left.size();
   }
 
   return 1 + this.left.size() + this.right.size();
-}
-
+};
 
 module.exports = {
   Queue,
   Node,
   LinkedList,
-  BinarySearchTree
+  BinarySearchTree,
 };
